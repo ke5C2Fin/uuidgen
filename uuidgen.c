99a06601d10b39ea7971973469c3d90bd1358523
Kevin Harrington <3482852+ke5C2Fin@users.noreply.github.com>
2025-10-16 12:08:43 -0600

    Remove OpenBSD ifdefs from uuidgen.c
    Removed OpenBSD specific code related to pledge and unveil.

--- uuidgen.c
+++ uuidgen.c
@@ -2,10 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <uuid.h>
-
-#ifdef __OpenBSD__
 #include <unistd.h>
-#endif
 
 #define FREE(p) { free(p); p = NULL; }
 
@@ -16,13 +13,11 @@ main(int argc, char *argv[])
 	uint32_t status;
 	char *str = NULL;
 
-#ifdef __OpenBSD__
 	if (pledge("stdio", NULL) == -1)
 		err(1, "pledge");
 	
 	if (unveil(NULL, NULL) == -1)
 		err(1, "unveil");
-#endif
 
 	(void)uuid_create(&uuid, &status);
 	if (status != uuid_s_ok)
