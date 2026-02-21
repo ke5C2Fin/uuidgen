dcab093426d28878d52f46967f605cf5f5a3d870
Kevin Harrington <3482852+ke5C2Fin@users.noreply.github.com>
2026-02-21 10:41:06 -0700

    Reorder pledge and unveil calls for uuidgen

--- uuidgen.c
+++ uuidgen.c
@@ -12,12 +12,12 @@ main(int argc, char *argv[])
 	uuid_t uuid;
 	uint32_t status;
 	char *str = NULL;
-
-	if (pledge("stdio", NULL) == -1)
-		err(1, "pledge");
 	
 	if (unveil(NULL, NULL) == -1)
 		err(1, "unveil");
+	
+	if (pledge("stdio", NULL) == -1)
+		err(1, "pledge");
 
 	(void)uuid_create(&uuid, &status);
 	if (status != uuid_s_ok)
